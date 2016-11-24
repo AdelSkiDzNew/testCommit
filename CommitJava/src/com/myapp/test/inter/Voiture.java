ad8838f6ccd17709992a78ec79f5224fa221dbdd
adel <adelautomatique@yahoo.fr>
2016-11-24 23:01:02 +0100

    commit class generic

---
+++ CommitJava/src/com/myapp/test/inter/Voiture.java
@@ -0,0 +1,21 @@
+package com.myapp.test.inter;
+
+
+import java.util.List;
+
+
+
+public abstract class Voiture <T>{
+
+	public void add3(T x){
+		
+	}
+
+	void add24(T x){
+		
+	}
+
+	public List<T> getAll(){
+		return null;
+	}
+}
