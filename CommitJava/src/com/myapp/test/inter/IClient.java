ddda3c751c8517056d9a4f2de8db3168a6f0968c
adel <adelautomatique@yahoo.fr>
2016-11-24 23:06:18 +0100

    IClient

--- CommitJava/src/com/myapp/test/inter/IClient.java
+++ CommitJava/src/com/myapp/test/inter/IClient.java
@@ -2,14 +2,18 @@ package com.myapp.test.inter;
 
 import java.util.List;
 
-
 public interface IClient {
 
 	void add(int x);
-	
+
 	void add2(String x);
-	
+
 	List<Integer> getAll();
-	
-	
+
+	void add5(int x);
+
+	void add6(String x);
+
+	List<Integer> getAll5();
+
 }
