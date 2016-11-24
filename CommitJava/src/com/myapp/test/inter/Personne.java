2ef7669193c20c5e3b53c81b9809decf997f1a29
adel <adelautomatique@yahoo.fr>
2016-11-24 23:04:25 +0100

    commit

---
+++ CommitJava/src/com/myapp/test/inter/Personne.java
@@ -0,0 +1,31 @@
+package com.myapp.test.inter;
+
+public class Personne <T> {
+  
+	private T t;
+	private T x;
+	private T y;
+	public Personne() {
+		// TODO Auto-generated constructor stub
+	}
+	public T getT() {
+		return t;
+	}
+	public void setT(T t) {
+		this.t = t;
+	}
+	public T getX() {
+		return x;
+	}
+	public void setX(T x) {
+		this.x = x;
+	}
+	public T getY() {
+		return y;
+	}
+	public void setY(T y) {
+		this.y = y;
+	}
+	
+	
+}
