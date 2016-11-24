e41811aff97e78b21f4407865337bdaaafabd089
adel <adelautomatique@yahoo.fr>
2016-11-24 23:12:25 +0100

    changement

---
+++ CommitJava/src/com/myapp/test/inter/IclientImpl.java
@@ -0,0 +1,25 @@
+package com.myapp.test.inter;
+
+import java.util.List;
+
+public class IclientImpl implements IClient {
+
+	@Override
+	public void add(int x) {
+		System.out.println(x);
+		
+	}
+
+	@Override
+	public void add2(String x) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Integer> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
